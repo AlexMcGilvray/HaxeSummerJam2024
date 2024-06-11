168b3cccf4c1ac3c6b26ac20091c08dd79d27c61
alex <alex@levelism.com>
2024-06-10 17:22:05 -0700

    basic tilemap world rendering now

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,6 @@
 package;
 
+import systems.World;
 import systems.CameraManager;
 import systems.GameHUD;
 import flixel.FlxState;
@@ -7,14 +8,17 @@ import flixel.FlxState;
 class PlayState extends FlxState {
 	var gameHUD:GameHUD;
 	var cameraManager:CameraManager;
+	var world:World;
 
 	override public function create():Void {
 		super.create();
 		cameraManager = new CameraManager();
 		gameHUD = new GameHUD();
+		world = new World();
 
 		cameraManager.registerWithUICamera(gameHUD);
 
+		add(world);
 		add(gameHUD);
 	}
 
---
+++ source/systems/World.hx
@@ -0,0 +1,26 @@
+package systems;
+
+import openfl.utils.Assets;
+import flixel.tile.FlxTilemap;
+import flixel.group.FlxGroup.FlxTypedGroup;
+
+class World extends FlxTypedGroup<FlxTilemap> {
+	var worldTileMap:FlxTilemap;
+
+	public function new() {
+		super();
+		worldTileMap = new FlxTilemap();
+
+		var levelJsonText = Assets.getText("assets/world.json");
+		var levelJson:Dynamic = haxe.Json.parse(levelJsonText);
+		var mapData:Array<Int> = levelJson.layers[0].data;
+		// Tilemap seems to be off by one for my purposes, more on why here :
+		// https://discourse.mapeditor.org/t/array-files-are-one-number-off-from-tile-set/1884
+		for (i in 0...mapData.length - 1) {
+			mapData[i] = mapData[i] - 1;
+		}
+		// load map from our csv data
+		worldTileMap.loadMapFromArray(mapData, levelJson.width, levelJson.height, "assets/images/tilemap_world.png", 16, 16, OFF, 0, 0, 65);
+		add(worldTileMap);
+	}
+}
