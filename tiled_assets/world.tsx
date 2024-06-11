7813c4d9a1f4eeb5e60acf00a38f244fd3f13287
alex <alex@levelism.com>
2024-06-10 18:59:28 -0700

    working on making a terrain brush for the world

--- tiled_assets/world.tsx
+++ tiled_assets/world.tsx
@@ -1,4 +1,26 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="world" tilewidth="16" tileheight="16" tilecount="256" columns="16">
  <image source="../assets/images/tilemap_world.png" width="256" height="256"/>
+ <wangsets>
+  <wangset name="Grass" type="corner" tile="-1">
+   <wangcolor name="light" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="dark" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="16" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="48" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="49" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="50" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="51" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="52" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="53" wangid="0,1,0,2,0,1,0,2"/>
+   <wangtile tileid="54" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="64" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="65" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="66" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="67" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="68" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="69" wangid="0,2,0,1,0,2,0,1"/>
+   <wangtile tileid="70" wangid="0,1,0,1,0,2,0,2"/>
+  </wangset>
+ </wangsets>
 </tileset>
